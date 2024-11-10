e1ca01e0f9a116c7cc16b8751a3f0609cc49569e
Alan Marinho <kgometalenator@gmail.com>
2024-11-10 18:07:51 -0300

    feat: ensure transactions and subscriptions pages are protected

--- finance-manager/app/transactions/page.tsx
+++ finance-manager/app/transactions/page.tsx
@@ -3,10 +3,20 @@ import { transactionColumns } from "./_columns";
 import { DataTable } from "../_components/ui/data-table";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const TransactionsPage = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
 
+  if (!userId) {
+    redirect("/login");
+  }
   return (
     <div>
       <Navbar />
